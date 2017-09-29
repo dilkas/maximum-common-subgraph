eac5b5428a51bb99848def53da602b217071a348
Paulius Dilkas <paulius.dilkas@gmail.com>
2017-09-29 10:28:05 +0100

    Importing algorithms and data

---
+++ main.R
@@ -0,0 +1,12 @@
+require(llama)
+
+#data = input(read.csv("features.csv"), read.csv("times.csv"))
+data(satsolvers)
+data = satsolvers
+
+folds = cvFolds(data)
+model = classify(makeLearner("classif.randomForest"), folds)
+
+mean(misclassificationPenalties(data, vbs))
+mean(misclassificationPenalties(folds, model))
+mean(misclassificationPenalties(data, singleBest))
