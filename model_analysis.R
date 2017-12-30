1ff172a2e2879e7ace99cec85f538a304ca922af
Paulius Dilkas <paulius.dilkas@gmail.com>
2017-12-30 06:22:10 +0200

    New data. Also more work towards analysing labelled data.

--- model_analysis.R
+++ model_analysis.R
@@ -1,8 +1,12 @@
 library("randomForest")
 library("llama")
 
-model <- readRDS("models/unlabelled.rds")
+type <- "both_labels"
+p_values <- c(10, 15, 20, 25, 33, 50)
+
+model <- readRDS(paste0("models/", type, ".rds"))
 forest <- model[["models"]][[1]][["learner.model"]]
+data <- readRDS(paste0("models/", type, "_data.rds"))
 
 graph_feature_names <- c("vertices", "edges", "loops", "mean degree",
                          "max degree", "SD of degrees", "density", "connected",
@@ -12,70 +16,77 @@ selected_features <- c("vertices", "edges", "mean degree", "max degree",
                        "density", "mean distance", "max distance")
 full_feature_names <- c(paste("pattern", graph_feature_names),
                         paste("target", graph_feature_names),
-                        paste(selected_features, "ratio"))
+                        paste(selected_features, "ratio"), "labelling")
 rm("graph_feature_names", "selected_features")
 
 # From random forest
 
-png("dissertation/images/unlabelled_forest_errors.png", width = 446,
+png(paste0("dissertation/images/", type, "_forest_errors.png"), width = 446,
     height = 288)
 plot(forest, main = "")
-legend("right", c("OOB", "clique", "k\u2193", "McSplit", "McSplit\u2193"),
-       fill = 1:5, xpd = TRUE)
+legend("right", c("OOB", "clique", "McSplit", "McSplit\u2193"),
+       fill = 1:4, xpd = TRUE)
 dev.off()
 
 importance <- importance(forest)
 row.names(importance) <- full_feature_names
-png("dissertation/images/unlabelled_variable_importance.png", width = 446,
-    height = 526)
+png(paste0("dissertation/images/", type, "_variable_importance.png"),
+    width = 446, height = 526)
 dotchart(sort(importance[,1]), xlab = "Mean decrease in Gini index")
 dev.off()
 rm("importance")
 
-png("dissertation/images/unlabelled_var_used.png", width = 446, height = 526)
+png(paste0("dissertation/images/", type, "_var_used.png"), width = 446,
+    height = 526)
 par(mar = c(0, 10, 0, 5))
 barplot(sort(setNames(varUsed(forest), full_feature_names)), las = 2,
         horiz = TRUE)
 dev.off()
 
-png("dissertation/images/tree_sizes.png", width = 446, height = 288)
+png(paste0("dissertation/images/", type, "tree_sizes.png"), width = 446,
+    height = 288)
 hist(treesize(forest), main = "", xlab = "tree size")
 dev.off()
 
 margin <- margin(forest)
-png("dissertation/images/unlabelled_margin.png", width = 446, height = 288)
+png(paste0("dissertation/images/", type, "_margin.png"), width = 446,
+    height = 288)
 plot(margin, ylab = "margin")
 dev.off()
-png("dissertation/images/unlabelled_margin2.png", width = 446, height = 288)
+png(paste0("dissertation/images/", type, "_margin2.png"), width = 446,
+    height = 288)
 plot(margin, sort = FALSE, ylab = "margin")
 dev.off()
-png("dissertation/images/clique_hist.png", width = 446, height = 288)
+png(paste0("dissertation/images/", type, "_clique_hist.png"), width = 446,
+    height = 288)
 hist(subset(margin, attr(margin, "names") == "clique"), main = "clique",
      xlab = "margin")
 dev.off()
-png("dissertation/images/kdown_hist.png", width = 446, height = 288)
-hist(subset(margin, attr(margin, "names") == "kdown"), main = "k\u2193",
-     xlab = "margin")
-dev.off()
-png("dissertation/images/mcsplit_hist.png", width = 446, height = 288)
+#png(paste0("dissertation/images/", type, "_kdown_hist.png"), width = 446,
+#    height = 288)
+#hist(subset(margin, attr(margin, "names") == "kdown"), main = "k\u2193",
+#     xlab = "margin")
+#dev.off()
+png(paste0("dissertation/images/", type, "_mcsplit_hist.png"), width = 446,
+    height = 288)
 hist(subset(margin, attr(margin, "names") == "mcsplit"), main = "McSplit",
      xlab = "margin")
 dev.off()
-png("dissertation/images/mcsplitdown_hist.png", width = 446, height = 288)
+png(paste0("dissertation/images/", type, "_mcsplitdown_hist.png"), width = 446,
+    height = 288)
 hist(subset(margin, attr(margin, "names") == "mcsplitdown"),
      main = "McSplit\u2193", xlab = "margin")
 dev.off()
 rm("margin")
 
-data <- readRDS("models/unlabelled_data.rds")
-png("dissertation/images/mcsplit_partial.png", width = 446, height = 288)
-partialPlot(forest, data[["data"]], "target.stddeg", "mcsplit",
-            main = "McSplit\u2193", xlab = "target SD of degrees")
-dev.off()
-png("dissertation/images/clique_partial.png", width = 446, height = 288)
-partialPlot(forest, data[["data"]], "target.stddeg", "clique", main = "clique",
-            xlab = "target SD of degrees")
-dev.off()
+#png("dissertation/images/mcsplit_partial.png", width = 446, height = 288)
+#partialPlot(forest, data[["data"]], "target.stddeg", "mcsplit",
+#            main = "McSplit\u2193", xlab = "target SD of degrees")
+#dev.off()
+#png("dissertation/images/clique_partial.png", width = 446, height = 288)
+#partialPlot(forest, data[["data"]], "target.stddeg", "clique", main = "clique",
+#            xlab = "target SD of degrees")
+#dev.off()
 
 # ECDF
 
@@ -84,26 +95,32 @@ library(latticeExtra)
 
 costs <- read.csv("results/costs.csv", header = FALSE)
 colnames(costs) <- c("ID", "cost")
-labels <- c("McSplit\u2193", "Llama", "VBS")
+costs <- subset(costs, costs$ID %in% filtered_instances)
+costs <- costs[rep(seq_len(nrow(costs)), each = length(p_values)),]
+costs$labelling <- p_values
+costs$ID <- sprintf("%02d %s", costs$labelling, costs$ID)
+costs <- costs[, c("ID", "cost")]
 
+labels <- c("clique", "Llama", "VBS")
 times <- subset(data$data, T, c("ID", data$performance))
 times$vbs <- apply(times[,-1], 1, min)
 winning_algorithms <- model$predictions[model$predictions$score == 1,
                                         c("ID", "algorithm")]
 winning_algorithms$algorithm <- unlist(
-  lapply(winning_algorithms$algorithm, function(x) which(colnames(times) == x)))
+  lapply(winning_algorithms$algorithm,
+         function(x) which(colnames(times) == x)))
 times <- merge(times, winning_algorithms, by = "ID", all.x = TRUE)
 times$llama <- as.numeric(times[cbind(seq_along(times$algorithm),
                                       times$algorithm)])
 times <- merge(times, costs, by = c("ID"), all.x = TRUE)
 times$llama <- times$llama + times$cost
 
-#summary(times$llama < times$mcsplitdown)
+summary(times$llama < times$clique)
 # how often each algorithm was predicted
-#summary(model$predictions$algorithm[model$predictions$score == 1])
-#summary(as.factor(unlist(data$best))) # how often each algorithm won
+summary(model$predictions$algorithm[model$predictions$score == 1])
+summary(as.factor(unlist(data$best))) # how often each algorithm won
 png("dissertation/images/ecdf_unlabelled_llama.png", width = 446, height = 288)
-ecdfplot(~ mcsplitdown + llama + vbs, data = times,
+ecdfplot(~ clique + llama + vbs, data = times,
          auto.key = list(space = "right", text = labels), xlab = "Runtime (ms)",
          ylim = c(0.9, 1))
 dev.off()
@@ -118,10 +135,10 @@ sum(successes(data, singleBest))
 #mean(parscores(data, model))
 #mean(parscores(data, vbs))
 #mean(parscores(data, singleBest))
-#contributions(data)
+contributions(data)
 #png("dissertation/images/unlabelled_scatterplot1.png", width = 446,
 #    height = 288)
-#perfScatterPlot(parscores, model, vbs, cvFolds(data, stratify = TRUE), data) + xlab("Llama") + ylab("VBS")
+perfScatterPlot(parscores, model, vbs, cvFolds(data, stratify = TRUE), data) + xlab("Llama") + ylab("VBS")
 #dev.off()
 #png("dissertation/images/unlabelled_scatterplot2.png", width = 446,
 #    height = 288)
